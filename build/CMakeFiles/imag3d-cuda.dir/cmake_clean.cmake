ec6208bb29999d3a0c63045c2688b4abc05bad18
DenMuj <denismujo98@gmail.com>
2025-10-14 14:30:13 +0200

    added Cmake. Works for linux. Not tested yet for W11

---
+++ build/CMakeFiles/imag3d-cuda.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/imag3d-cuda.dir/src/imag3d-cuda/imag3d-cuda.cu.o"
+  "CMakeFiles/imag3d-cuda.dir/src/imag3d-cuda/imag3d-cuda.cu.o.d"
+  "imag3d-cuda"
+  "imag3d-cuda.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/imag3d-cuda.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/real3d-cuda.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/real3d-cuda.dir/src/real3d-cuda/real3d-cuda.cu.o"
+  "CMakeFiles/real3d-cuda.dir/src/real3d-cuda/real3d-cuda.cu.o.d"
+  "real3d-cuda"
+  "real3d-cuda.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/real3d-cuda.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
